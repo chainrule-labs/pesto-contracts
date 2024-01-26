cad4b4aba89df29fcd51f6778f66b106ba900245
cucupac <46691282+cucupac@users.noreply.github.com>
2024-01-26 13:01:39 -0600

    feat: add ERC712 permit to functions with transferFrom

--- src/interfaces/IPosition.sol
+++ src/interfaces/IPosition.sol
@@ -43,6 +43,30 @@ interface IPosition {
         payable;
 
     /**
+     * @notice Adds to this contract's short position with permit, obviating the need for a separate approve tx.
+     * @param _cAmt The amount of collateral to be supplied for this transaction-specific loan (units: C_DECIMALS).
+     * @param _ltv The desired loan-to-value ratio for this transaction-specific loan (ex: 75 is 75%).
+     * @param _swapAmtOutMin The minimum amount of output tokens from swap for the tx to go through.
+     * @param _poolFee The fee of the Uniswap pool.
+     * @param _client The address, controlled by client operators, for receiving protocol fees (use address(0) if no client).
+     * @param _deadline The deadline timestamp that the permit is valid.
+     * @param _v The V parameter of ERC712 permit signature.
+     * @param _r The R parameter of ERC712 permit signature.
+     * @param _s The S parameter of ERC712 permit signature.
+     */
+    function shortWithPermit(
+        uint256 _cAmt,
+        uint256 _ltv,
+        uint256 _swapAmtOutMin,
+        uint24 _poolFee,
+        address _client,
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external payable;
+
+    /**
      * @notice Fully closes the short position.
      * @param _poolFee The fee of the Uniswap pool.
      * @param _exactOutput Whether to swap exact output or exact input (true for exact output, false for exact input).
@@ -60,12 +84,44 @@ interface IPosition {
     function addCollateral(uint256 _cAmt) external payable;
 
     /**
+     * @notice Increases the collateral amount for this contract's loan with permit, obviating the need for a separate approve tx.
+     * @param _cAmt The amount of collateral to be supplied (units: C_DECIMALS).
+     * @param _deadline The deadline timestamp that the permit is valid.
+     * @param _v The V parameter of ERC712 permit signature.
+     * @param _r The R parameter of ERC712 permit signature.
+     * @param _s The S parameter of ERC712 permit signature.
+     */
+    function addCollateralWithPermit(uint256 _cAmt, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s)
+        external
+        payable;
+
+    /**
      * @notice Repays any outstanding debt to Aave and transfers remaining collateral from Aave to owner.
      * @param _dAmt The amount of debt token to repay to Aave (units: D_DECIMALS).
      *              To pay off entire debt, _dAmt = debtOwed + smallBuffer (to account for interest).
      * @param _withdrawBuffer The amount of collateral left as safety buffer for tx to go through (default = 100_000, units: 8 decimals).
      */
     function repayAfterClose(uint256 _dAmt, uint256 _withdrawBuffer) external payable;
+
+    /**
+     * @notice Repays any outstanding debt to Aave and transfers remaining collateral from Aave to owner,
+     *         with permit, obviating the need for a separate approve tx.
+     * @param _dAmt The amount of debt token to repay to Aave (units: D_DECIMALS).
+     *              To pay off entire debt, _dAmt = debtOwed + smallBuffer (to account for interest).
+     * @param _withdrawBuffer The amount of collateral left as safety buffer for tx to go through (default = 100_000, units: 8 decimals).
+     * @param _deadline The deadline timestamp that the permit is valid.
+     * @param _v The V parameter of ERC712 permit signature.
+     * @param _r The R parameter of ERC712 permit signature.
+     * @param _s The S parameter of ERC712 permit signature.
+     */
+    function repayAfterCloseWithPermit(
+        uint256 _dAmt,
+        uint256 _withdrawBuffer,
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external payable;
 
     /* ****************************************************************************
     **
