d9cd3616dc797e7a6e39e4678462104e87e79b3b
Adam Cuculich <46691282+cucupac@users.noreply.github.com>
2024-03-19 22:49:51 -0500

    refactor: move protocol fee to fee collector (#24)

--- src/libraries/FeeLib.sol
+++ src/libraries/FeeLib.sol
@@ -9,10 +9,6 @@ import { IFeeCollector } from "src/interfaces/IFeeCollector.sol";
 /// @notice Manages all protocol-fee-related interactions
 library FeeLib {
     // Constants: no SLOAD to save gas
-
-    /// @notice The protocol fee rate.
-    uint256 public constant PROTOCOL_FEE_RATE = 3;
-
     /// @notice The address of the fee collector contract.
     address public constant FEE_COLLECTOR = 0x7A7AbDb9E12F3a9845E2433958Eef8FB9C8489Ee;
 
@@ -22,7 +18,7 @@ library FeeLib {
     /// @param _client The address where a client operator will receive protocols fees.
     /// @return cAmtNet The resulting amount of collateral to be supplied after fees are taken.
     function takeProtocolFee(address _token, uint256 _amt, address _client) internal returns (uint256 cAmtNet) {
-        uint256 maxFee = (_amt * PROTOCOL_FEE_RATE) / 1000;
+        uint256 maxFee = (_amt * IFeeCollector(FEE_COLLECTOR).feeRate()) / 1000;
         (uint256 userSavings, uint256 clientFee) = IFeeCollector(FEE_COLLECTOR).getClientAllocations(_client, maxFee);
         uint256 totalFee = maxFee - userSavings;
         cAmtNet = _amt - totalFee;
